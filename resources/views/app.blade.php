8df1c1c0b8ad325a3fe4f1e16266d1b14ceeb7d8
Dion Craftinger <dcraftinger@gmail.com>
2023-12-08 14:27:58 +0300

    Define the appName as the shared inertia prop and use it in the app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $page['props']['appName'] ?? '' }}</title>
 
     @routes
     @vite('resources/js/app.js')
